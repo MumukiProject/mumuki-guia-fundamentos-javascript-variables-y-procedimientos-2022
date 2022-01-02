bf662452c016a98b1d26d3a29715e05eb02476b6
franco@mumuki.org <franco@mumuki.org>
2022-01-02 07:34:16 +0000

    Mumuki Export on 2022-01-02 07:34:16 +0000

--- 00006_¿Y el tablero_/extra.js
+++
@@ -1,7 +0,0 @@
-function funcionMisteriosa(n, m) {
-  return 'w' + repeatChar('o', n) + repeatChar('w', m) + '!';
-}
-
-function repeatChar(c, n) {
-  return new Array(n).fill(c).join('');
-}
--- 00009_Impresión por pantalla/extra.js
+++
@@ -1,5 +0,0 @@
-function funcionEgocentrica() {
-  imprimir("soy una función que imprime por pantalla");
-  imprimir("y estoy por devolver el valor 5");
-  return 5;
-}
