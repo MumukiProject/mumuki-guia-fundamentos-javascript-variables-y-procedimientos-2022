d58d14ce43dca65e2db080961622ea22ff913802
franco@mumuki.org <franco@mumuki.org>
2022-01-03 00:44:14 +0000

    Mumuki Export on 2022-01-03 00:44:14 +0000

--- 00021_Se enfrió el agua/test..js
+++ 00021_Se enfrió el agua/test..js
@@ -2,12 +2,12 @@ describe("cambiar el agua", function() {
   it("vaciarTermo deja en 0 el aguaDelTermo", () => {    
     aguaDelTermo = 1000;
     vaciarTermo();
-    assert.equals(aguaDelTermo, 0);
+    assert.equal(aguaDelTermo, 0);
   });
 
   it("llenarTermo deja en 1000 el aguaDelTermo", () => {
     aguaDelTermo = 80;
     llenarTermo();
-    assert.equals(aguaDelTermo, 1000);
+    assert.equal(aguaDelTermo, 1000);
   });
 })
